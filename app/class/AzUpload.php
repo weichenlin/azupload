610d40628a1339559cfafe47d0a8019cd20c5ff2
weichenlin <wclinhub@sp36.net>
2015-05-19 00:10:34 +0800

    fix download big file OOM

--- app/class/AzUpload.php
+++ app/class/AzUpload.php
@@ -120,7 +120,20 @@ class FileManager
     public function sendFilecontent($saveName)
     {
         $file = FILE_STORE . $saveName;
-        readfile($file);
+        if ($this->getFileSize($saveName) > 4096) {
+            $srcStream = fopen($file, 'rb');
+            $dstStream = fopen('php://output', 'wb');
+
+            $offset = 0;
+            while(!feof($srcStream)) {
+                $offset += stream_copy_to_stream($srcStream, $dstStream, 4096, $offset);
+            }
+
+            fclose($dstStream);
+            fclose($srcStream);
+        } else {
+            readfile($file);
+        }
     }
 
     public function getFileSize($saveName)
